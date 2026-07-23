59955a1fe45b2d02e657831747d737423b962ee6
Anuska111 <anuska.biswas.mec24@gmail.com>
2026-07-23 23:29:09 +0530

    Add backend files and ignore node_modules

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ document.querySelector("input").value="";
 }
 
 }
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(3000,()=>{
+    console.log("Server Running");
+});
